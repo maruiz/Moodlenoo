4289b986dbc12bec8d67a4abb86938e07e8b428b
Evenie Chao <eveniehorton@gmail.com>
2014-06-23 11:26:11 -0500

    added course scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140619140217) do
+ActiveRecord::Schema.define(version: 20140623162336) do
+
+  create_table "courses", force: true do |t|
+    t.string   "title"
+    t.string   "course_number"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "students", force: true do |t|
     t.string   "name"
